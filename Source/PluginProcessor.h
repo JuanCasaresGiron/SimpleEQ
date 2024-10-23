fdba51c2646839e37036ab13dd15058054aa49d3
Juan Casares <juan.casares5@gmail.com>
2024-10-23 11:46:27 -0400

    fixing some merging errors
    fixing  some merging errors

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -93,7 +93,7 @@ private:
         HighCut
 
     };
-
+    void updatePeakFilter(const ChainSettings& chainSettings);
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
